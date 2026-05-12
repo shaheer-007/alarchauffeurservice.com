3381730ed030df84dd22e0d1301d2c9ce8158f13
shaheer-007 <shaheer.siddiqui@bmglobalinc.com>
2026-05-12 22:49:11 +0500

    blocked spam crawlers and the urls containing word 'items'

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (Request::has('item')) {
+            throw new NotFoundHttpException();
+        }
     }
 }
